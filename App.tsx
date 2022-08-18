eb282b4d74598facdb21c221554de57446261989
tolgahan-arikan <tolgahan.arikan@gmail.com>
2022-08-18 21:47:07 +0300

    Adjust server path for real device

--- App.tsx
+++ App.tsx
@@ -7,14 +7,16 @@ import RNFetchBlob from 'rn-fetch-blob';
 var RNFS = require('react-native-fs');
 
 const path = RNFS.MainBundlePath + '/www';
-const server = new StaticServer(8080, path);
+const documentPath = RNFS.DocumentDirectoryPath + '/www';
+RNFS.copyFile(path, documentPath);
+let server;
 
 const App = () => {
   const [url, setUrl] = useState<string | undefined>(undefined);
   const [loaded, setLoaded] = useState<boolean>(false);
   useEffect(() => {
-    console.log(RNFS.readDir(path));
-    RNFetchBlob.config({path: path + '/image.jpg'})
+    console.log(RNFS.readDir(documentPath));
+    RNFetchBlob.config({path: documentPath + '/image.jpg'})
       .fetch(
         'GET',
         'https://assets.skyweaver.net/aL_BvVlm/webapp/backgrounds/private.jpg',
@@ -22,18 +24,24 @@ const App = () => {
       .then(res => {
         console.log('file saved to path:', res.path());
         setLoaded(true);
+        server = new StaticServer(8080, documentPath);
+        server.start().then(url => {
+          setUrl(url);
+        });
       });
-    server.start().then(url => {
-      setUrl(url);
-    });
   }, []);
   return (
     <SafeAreaView>
       {!loaded && <Text>Downloading and saving image</Text>}
       {url && loaded && (
-        <View style={{height: '100%', width: '100%'}}>
+        <View style={{height: '100%', width: '100%', backgroundColor: 'white'}}>
           <Text>Serving {url}</Text>
-          <WebView style={{flex: 1}} source={{uri: url}} />
+          <WebView
+            style={{flex: 1}}
+            source={{uri: url}}
+            allowFileAccess
+            allowingReadAccessToURL={url}
+          />
         </View>
       )}
     </SafeAreaView>
